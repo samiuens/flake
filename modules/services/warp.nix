33ef4c60044c802b984c5a49ee321afe5e7f9d0e
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-05 18:03:33 +0200

    ☁️ feat(modules/services): add cloudflare warp module

---
+++ modules/services/warp.nix
@@ -0,0 +1,8 @@
+{ config, lib, pkgs, ... }:
+{
+  options.smi.services.warp.enable = lib.mkEnableOption "Cloudflare WARP";
+
+  config = lib.mkIf config.smi.services.warp.enable {
+    services.cloudflare-warp.enable = true;
+  };
+}
