b33b9fb11cfd654a33dfc61b01e08346cf00a86b
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-05 18:02:52 +0200

    🔗 feat(modules/common): add home-manager and disko integration

---
+++ modules/common/home-manager.nix
@@ -0,0 +1,20 @@
+{ inputs, helpers, ... }:
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+    inputs.disko.nixosModules.disko
+    inputs.nix-flatpak.nixosModules.nix-flatpak
+  ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = { inherit inputs helpers; };
+    backupFileExtension = ".bak";
+    sharedModules = [
+      (import ../../home)
+      inputs.nix-index-database.homeModules.default
+      inputs.nix-flatpak.homeManagerModules.nix-flatpak
+    ];
+  };
+}
