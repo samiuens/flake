0de3a6aa0c41199629fabeadba5acc2d14f87ab2
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-05 18:03:02 +0200

    🎮 feat(modules/hardware): add gpu module

---
+++ modules/hardware/gpu.nix
@@ -0,0 +1,40 @@
+{ config, lib, pkgs, ... }:
+{
+  options.smi.hardware.gpu = lib.mkOption {
+    type = lib.types.enum [
+      "amd"
+      "intel"
+      "nvidia"
+      "unknown"
+    ];
+    default = "unknown";
+    description = "GPU type; \"unknown\" applies no specific configuration";
+  };
+
+  config = lib.mkMerge [
+    (lib.mkIf (config.smi.hardware.gpu == "amd") {
+      hardware.graphics = {
+        enable = true;
+        enable32Bit = true;
+      };
+    })
+    (lib.mkIf (config.smi.hardware.gpu == "intel") {
+      hardware.graphics = {
+        enable = true;
+        enable32Bit = true;
+        extraPackages = [ pkgs.intel-media-driver ];
+      };
+    })
+    (lib.mkIf (config.smi.hardware.gpu == "nvidia") {
+      hardware.graphics.enable = true;
+      hardware.nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = lib.mkDefault false;
+        open = lib.mkDefault false;
+        nvidiaSettings = lib.mkDefault true;
+        package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.stable;
+      };
+      services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
+    })
+  ];
+}
