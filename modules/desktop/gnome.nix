add7dbd5c7f841b2a7f5a9e6aa90d20315f466e5
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-05 18:03:11 +0200

    🖥️ feat(modules/desktop): add gnome module

---
+++ modules/desktop/gnome.nix
@@ -0,0 +1,57 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
+  config = lib.mkIf (config.smi.desktop.enable && config.smi.desktop.environment == "gnome") {
+    services.desktopManager.gnome.enable = true;
+    services.displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
+
+    programs.dconf.enable = true;
+    environment = {
+      systemPackages = with pkgs; [
+        dconf-editor
+      ];
+      gnome.excludePackages = with pkgs; [
+        gnome-photos
+        gnome-music
+        totem
+        cheese
+
+        epiphany
+        gnome-maps
+        gnome-weather
+        gnome-contacts
+        gnome-online-accounts
+
+        gnome-calendar
+        gnome-clocks
+        gnome-characters
+        gnome-font-viewer
+        gnome-logs
+        simple-scan
+        yelp
+        gnome-tour
+        gnome-user-docs
+        snapshot
+        gnome-terminal
+      ];
+      sessionVariables = {
+        NIXOS_OZONE_WL = "1";
+        QT_QPA_PLATFORM = "wayland;xcb";
+      };
+    };
+    documentation.nixos.enable = false;
+
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      config.common.default = "gnome";
+    };
+  };
+}
