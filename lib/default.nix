0064bfd9e84b39420ca4b129377098eb1dcaaa65
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-06 11:42:17 +0200

    ♻️ refactor(lib): add mkSimpleProgram and mkGitProvider factory functions
    mkSimpleProgram generates a standard enable-option + home.packages module
    from a name and package list. mkGitProvider does the same for git hosting
    providers that wire into smi.programs.ssh.providers.

--- lib/default.nix
+++ lib/default.nix
@@ -22,4 +22,52 @@
         );
     in
     go dir true;
+
+  mkSimpleProgram =
+    {
+      config,
+      lib,
+      name,
+      packages,
+    }:
+    {
+      options.smi.programs.${name}.enable = lib.mkEnableOption name;
+      config = lib.mkIf config.smi.programs.${name}.enable {
+        home.packages = packages;
+      };
+    };
+
+  mkGitProvider =
+    {
+      config,
+      lib,
+      name,
+      displayName,
+      defaultInsteadOf,
+    }:
+    let
+      cfg = config.smi.programs.git.${name};
+    in
+    {
+      options.smi.programs.git.${name} = {
+        enable = lib.mkEnableOption displayName;
+        username = lib.mkOption {
+          type = lib.types.str;
+          default = "";
+          description = "${displayName} username";
+        };
+        insteadOf = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = [ ];
+          description = "URLs/prefixes to rewrite to SSH";
+        };
+      };
+      config = lib.mkIf cfg.enable {
+        smi.programs.ssh.providers.${name} = {
+          enable = true;
+          inherit (cfg) username;
+          insteadOf = defaultInsteadOf ++ cfg.insteadOf;
+        };
+      };
+    };
 }
