953b168f2988d6e536169054571eadc0eedd9e5e
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-08 00:50:21 +0200

    ♻️ refactor(vscodium): expose profiles option
    Drop the hardcoded typst profile and font options; pass a generic
    profiles attrset through to programs.vscode.profiles so consumers
    can define their own profiles.

--- home/programs/development/vscodium.nix
+++ home/programs/development/vscodium.nix
@@ -12,30 +12,10 @@ in
   options.smi.programs.${name} = {
     enable = lib.mkEnableOption name;
 
-    editor = {
-      fontFamily = lib.mkOption {
-        type = lib.types.str;
-        default = "'JetBrains Mono', 'JetBrainsMono Nerd Font', monospace";
-        description = "Editor font family";
-      };
-      fontSize = lib.mkOption {
-        type = lib.types.int;
-        default = 16;
-        description = "Editor font size";
-      };
-    };
-
-    terminal = {
-      fontFamily = lib.mkOption {
-        type = lib.types.str;
-        default = "JetBrainsMono Nerd Font";
-        description = "Integrated terminal font family";
-      };
-      fontSize = lib.mkOption {
-        type = lib.types.int;
-        default = 14;
-        description = "Integrated terminal font size";
-      };
+    profiles = lib.mkOption {
+      type = lib.types.attrsOf lib.types.anything;
+      default = { };
+      description = "VSCodium profiles (extensions and user settings)";
     };
   };
 
@@ -43,34 +23,8 @@ in
     programs.vscode = {
       enable = true;
       package = pkgs.vscodium;
-
       mutableExtensionsDir = false;
-
-      profiles = {
-        "typst" = {
-          extensions = with pkgs.vscode-extensions; [
-            jnoortheen.nix-ide
-            myriad-dreamin.tinymist
-            mkhl.direnv
-            pkief.material-icon-theme
-            editorconfig.editorconfig
-            tomoki1207.pdf
-          ];
-
-          userSettings = {
-            "workbench.iconTheme" = "material-icon-theme";
-            "editor.fontFamily" = cfg.editor.fontFamily;
-            "editor.fontSize" = cfg.editor.fontSize;
-            "editor.lineHeight" = 2;
-            "files.autoSave" = "onFocusChange";
-            "editor.cursorStyle" = "block";
-            "terminal.integrated.fontFamily" = cfg.terminal.fontFamily;
-            "terminal.integrated.fontSize" = cfg.terminal.fontSize;
-            "telemetry.telemetryLevel" = "off";
-            "update.mode" = "none";
-          };
-        };
-      };
+      inherit (cfg) profiles;
     };
   };
 }
