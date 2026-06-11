8c69800cb5281749377335ba7de4b8bc6d6d023a
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-11 10:48:46 +0200

    ♻️ refactor(flatpak): drop platform tag, gate behind isLinux

--- home/services/flatpak.nix
+++ home/services/flatpak.nix
@@ -1,32 +1,29 @@
 {
-  platforms = [ "linux" ];
-  module =
-    {
-      config,
-      lib,
-      ...
-    }:
-    let
-      cfg = config.smi.services.flatpak;
-    in
-    {
-      options.smi.services.flatpak = {
-        packages = lib.mkOption {
-          type = lib.types.listOf (lib.types.either lib.types.str lib.types.attrs);
-          default = [ ];
-          example = [
-            "com.spotify.Client"
-            {
-              appId = "us.zoom.Zoom";
-              origin = "flathub";
-            }
-          ];
-          description = "User-scoped Flatpaks to install";
-        };
-      };
-
-      config = {
-        services.flatpak.packages = cfg.packages;
-      };
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.smi.services.flatpak;
+in
+{
+  options.smi.services.flatpak = {
+    packages = lib.mkOption {
+      type = lib.types.listOf (lib.types.either lib.types.str lib.types.attrs);
+      default = [ ];
+      example = [
+        "com.spotify.Client"
+        {
+          appId = "us.zoom.Zoom";
+          origin = "flathub";
+        }
+      ];
+      description = "User-scoped Flatpaks to install";
     };
+  };
+
+  config = lib.mkIf pkgs.stdenv.isLinux {
+    services.flatpak.packages = cfg.packages;
+  };
 }
