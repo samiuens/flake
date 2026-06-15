0f27dc28a8bbd9ddf900f1dc538134ec55f25e6d
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-15 09:50:30 +0200

    ♻️ refactor(zed): strip personal settings

--- home/programs/development/zed.nix
+++ home/programs/development/zed.nix
@@ -8,77 +8,9 @@ let
   cfg = config.smi.programs.${name};
 in
 {
-  options.smi.programs.${name} = {
-    enable = lib.mkEnableOption name;
-
-    bufferFont = {
-      family = lib.mkOption {
-        type = lib.types.str;
-        default = "JetBrains Mono";
-        description = "Editor buffer font family";
-      };
-      size = lib.mkOption {
-        type = lib.types.int;
-        default = 16;
-        description = "Editor buffer font size";
-      };
-    };
-
-    terminal = {
-      fontFamily = lib.mkOption {
-        type = lib.types.str;
-        default = "JetBrainsMono Nerd Font";
-        description = "Integrated terminal font family";
-      };
-      fontSize = lib.mkOption {
-        type = lib.types.int;
-        default = 14;
-        description = "Integrated terminal font size";
-      };
-    };
-  };
+  options.smi.programs.${name}.enable = lib.mkEnableOption name;
 
   config = lib.mkIf cfg.enable {
-    programs.zed-editor = {
-      enable = true;
-      extensions = [
-        "material-icon-theme"
-        "nix"
-      ];
-      userSettings = {
-        icon_theme = "Material Icon Theme";
-        ui_font_family = ".ZedSans";
-        ui_font_size = 16;
-
-        autosave = "on_focus_change";
-        cursor_shape = "block";
-        buffer_font_family = cfg.bufferFont.family;
-        buffer_font_size = cfg.bufferFont.size;
-
-        terminal = {
-          font_family = cfg.terminal.fontFamily;
-          font_size = cfg.terminal.fontSize;
-        };
-
-        features.copilot = false;
-        disable_ai = true;
-
-        telemetry.metrics = false;
-
-        load_direnv = "shell_hook";
-        lsp.nixd.binary.path = "nixd";
-        languages.Nix = {
-          language_servers = [
-            "nixd"
-            "!nil"
-          ];
-          formatter.external = {
-            command = "nixfmt";
-            arguments = [ "-q" ];
-          };
-          format_on_save = "on";
-        };
-      };
-    };
+    programs.zed-editor.enable = true;
   };
 }
